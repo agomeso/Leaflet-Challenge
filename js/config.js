3e37fda1b0d9e7ca663b5e6bd7b599c90a284af2
agomeso <alejandrogomezmba@gmail.com>
2021-05-12 21:39:35 -0500

    commit

--- js/config.js
+++ js/config.js
@@ -1,2 +1,3 @@
 // API key
-const API_KEY = "pk.eyJ1IjoiYWdvbWVzbyIsImEiOiJja29hcWtlOXQwYjMzMm9uMm93dXU5bWJ2In0.Fr838EFUtlRffpy1wLhmcw";
+const API_KEY = "pk.eyJ1IjoiYWdvbWVzbyIsImEiOiJja29hcWtlOXQwYjMzMm9uMm93dXU5bWJ2In0.Fr838EFUtlRffpy1wLhmcw"
+
